6a3bf4005acdf8b614efb7c3939b6d0827c4cf75
ZohrehGit <seyedeh22001@student.hamk.fi>
2023-02-01 10:53:57 +0200

    Task1- 01-02-2023

--- layout/Menu.php
+++ layout/Menu.php
@@ -2,9 +2,12 @@
 $title = "menu";
 include "header.php" ?>
 
-    <h1>3.1 My Simple  PHP Script</h1>
+    <h1>Menu</h1>
 
-    <h4>Current date</h4>
+    <h4>simple menu</h4>
 
-    <?php echo date("m.d.Y");?>
+    <h1>Current date</h1>
+
+    <?php echo date('l jS \of F Y h:i:s A');?>
+    
     <?php include "footer.php" ?>
