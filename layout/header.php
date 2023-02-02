d0e52dc98411b3a183e561861daba1e2425027f2
ZohrehGit <seyedeh22001@student.hamk.fi>
2023-02-02 19:47:55 +0200

    Main Page - C

--- layout/header.php
+++ layout/header.php
@@ -52,9 +52,14 @@
   </div>
 </nav>
 </div> 
+
   <div class="row" style="margin-top: 10px; margin-bottom: 10px; margin-right: 5px;">
+  <!--
     <div class="col-sm-3" style="background-color: rgba(228, 235, 194, 0.353);">
       <h3>List of Tasks</h3>
       some thing here
     </div>
+  -->
     <div class="col-sm-9">
+      <!--
+      -->
--- layout/index.php
+++ layout/index.php
@@ -2,6 +2,18 @@
 $title = "Cool Resturant";
 include "header.php" ?>
 
+<div class="content">
+        
+        <article class="content_info">
+            <P>
+                <h1>Welcome to our Restaurant</h1>
+                <h4>Our promise is to provide great food and a stunning view of the city.<br>Everyone is welcome to enjoy our services.</h4>
+            </P>                    
+            <button>Read more...</button>
+        </article>
+        <img src="images/Meal-Plan-plate-protein.png" alt="Foodplate" width="250px">
+    </div>
+<!--
 <h1> Header 01-02-2023 </h1>
 <p1> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempore iste enim itaque molestias 
     aliquid placeat eligendi distinctio est architecto aperiam vel qui aspernatur perferendis 
@@ -9,5 +21,7 @@ include "header.php" ?>
 <p1> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui, harum? Dolor ad itaque 
     blanditiis esse eum nobis sunt eveniet mollitia praesentium, repudiandae molestias voluptas 
     os dolores similique maiores facere soluta? </p1>
+-->
+
 <?php include "footer.php" ?>
 
